e09d5e7b384157f3e6a64ac7207f407779dce1fa
unknown <etboyette@gmail.com>
2015-09-24 14:46:51 -0400

    Added Assignment_2_pt1.java

---
+++ Assignment_2_pt1.java
@@ -0,0 +1,26 @@
+
+public class Assignment_2_pt1 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.print("1/2=" + 1 / 2);
+	System.out.println(" (int)");
+	System.out.print("1%2=" + 1 % 2);
+	System.out.println(" (int)");
+	System.out.print("1.0/2=" + 1.0 / 2);
+	System.out.println(" (double)");
+	System.out.print("5+7/2=" + (5 + 7 / 2));
+	System.out.println(" (int)");
+	System.out.print("\"Beat\" + ' ' + \"Army\"=" + "Beat" + ' ' + "Army");
+	System.out.println(" (string)");
+	System.out.print("6+13/5-35%3=" + (6+13/5-35%3));
+	System.out.println(" (int)");
+	System.out.print("3.5*(5/4)=" + (3.5*(5/4)));
+	System.out.println(" (double)");
+	System.out.print("(3.5*5)/4=" + (3.5*5)/4);
+	System.out.println(" (double)");
+
+	}
+
+}
