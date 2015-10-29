604a868f77902088df85c862f261bcbc41b8d4d3
etboyette <etboyette@gmail.com>
2015-10-29 17:35:51 -0400

    Added Person.java

---
+++ workspace/Class Exercises/src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+	String Name;
+	int Age;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	
+	
+}
